f5aa7ea77fe80262e5035f707e1e029801bc8495
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-02 23:21:56 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -4,13 +4,19 @@ console.log("Task-2");
 console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
-
+Получить массив объектов пользователей по цвету глаз (поле eyeColor).
 */
 
 //Task START
 
+import users from './users.js'
 
+const getUsersWithEyeColor = (users, color) => {
+  // твой код
+  return users.filter(user => user.eyeColor === color);
+};
 
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 //Task END
 
