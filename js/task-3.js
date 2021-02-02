309f4629bb7835f326a40531112a2fddd86b108e
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-02 23:59:11 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -5,14 +5,21 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Получить массив имен пользователей по полу (поле gender).
 */
 
 // Task START
 
+import users from './users.js'
 
+const getUsersWithGender = (users, gender) => {
+  // твой код
 
+  return users.filter(users => users.gender === gender).map(users => users.name);
 
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 
 //Task END
