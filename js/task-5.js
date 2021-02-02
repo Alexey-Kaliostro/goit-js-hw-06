d738052977e4cd23a244f41b14e4d99fcfd9c12e
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-03 00:22:47 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -5,14 +5,20 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Получить пользоваля (не массив) по email (поле email, он уникальный).
 */
 
 // Task START
 
+import users from './users.js'
 
+const getUserWithEmail = (users, email) => {
+  // твой код
+  return users.find(users => users.email === email);
+};
 
-
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
 //Task END
 
