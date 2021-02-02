b931016ac8a722473b6f9f8dea2be79e36fee30a
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-02 22:58:18 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -5,15 +5,36 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Получить массив имен всех пользователей (поле name).
 */
 
 // Task START
 
+const getUserNames = users => {
+  // твой код
+  return users.map(user => user.name);
+ 
+};
 
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
 
 //Task END
+
+/*
+
+// Первый вариант
+const userNamesArr = users.map(function(user){
+  return user.name;
+});
+console.log(userNamesArr);
+
+// Второй вариант
+const userNamesArr2 = users.map(user => user.name);
+console.log(userNamesArr2);
+
+*/
 
 console.log(" ");
 console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
