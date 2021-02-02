851ae6c14c96149c799c1b8a36975e03a46d2a90
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-03 00:05:42 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -5,14 +5,19 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Получить массив только неактивных пользователей (поле isActive).
 */
 
 // Task START
 
+import users from './users.js'
 
+const getInactiveUsers = users => {
+  // твой код
+  return users.filter(users => !users.isActive);
+};
 
-
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
 //Task END
 
