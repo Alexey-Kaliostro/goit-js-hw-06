de883f6c532cd3b009c66e531c623b89c1747920
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-03 00:42:00 +0200

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -5,14 +5,23 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
 */
 
 // Task START
 
+import users from './users.js'
 
+const getUsersWithAge = (users, min, max) => {
+  // твой код
+  return users.filter(users => users.age >= min && users.age <= max);
 
+};
 
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 //Task END
 
