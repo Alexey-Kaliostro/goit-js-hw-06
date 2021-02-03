34e378a313027d63d10ccd36a630b4ac75641e00
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-03 20:07:49 +0200

    task-7

--- js/task-7.js
+++ js/task-7.js
@@ -5,14 +5,19 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Получить общую сумму баланса (поле balance) всех пользователей.
 */
 
 // Task START
 
+import users from './users.js'
 
+const calculateTotalBalance = users => {
+  // твой код
+  return users.reduce((totalBalance, user) => totalBalance + user.balance, 0)
+};
 
-
+console.log(calculateTotalBalance(users)); // 20916
 
 //Task END
 
