1947f06f060d015c16e612a44fec4b577f9d14f5
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-03 22:53:32 +0200

    task-8

--- js/task-8.js
+++ js/task-8.js
@@ -5,14 +5,20 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Массив имен всех пользователей у которых есть друг с указанным именем.
 */
 
 // Task START
 
+const getUsersWithFriend = (users, friendName) => {
+  
+  // твой код
+  return users.filter(user => user.friends.some(friend => friend === friendName)).map(user => user.name);
 
+   };
 
-
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
 //Task END
 
