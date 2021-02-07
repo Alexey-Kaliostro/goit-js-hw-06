d19169f0d6b28d2fa626b3365b136d10614c4455
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-07 14:55:50 +0200

    task-9

--- js/task-9.js
+++ js/task-9.js
@@ -5,14 +5,22 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
-
+Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 */
 
 // Task START
 
+const getNamesSortedByFriendsCount = users => {
+  // твой код
+  return [...users]
+  .sort((lessFriendsQty, moreFriendsQty) => lessFriendsQty.friends.length - moreFriendsQty.friends.length)
+  .map(user => user.name);
+    
+    
+};
 
-
-
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 //Task END
 
